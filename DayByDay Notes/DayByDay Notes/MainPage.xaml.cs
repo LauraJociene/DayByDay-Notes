c80c06afec9e091bc2a2391041055ffbd61d0ff6
tomas <tsmilgius@gmail.com>
2017-01-11 19:42:15 +0200

    test

--- DayByDay Notes/DayByDay Notes/MainPage.xaml.cs
+++ DayByDay Notes/DayByDay Notes/MainPage.xaml.cs
@@ -18,7 +18,7 @@ using Windows.UI.Xaml.Navigation;
 namespace DayByDay_Notes
 {
     /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// An empty page that can be used on its own or navigated to within a Frame. 
     /// </summary>
     public sealed partial class MainPage : Page
     {
